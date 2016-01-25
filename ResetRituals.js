d0175f09d4df96aeea88cc4f31445152be7a0a66
Elethom Hunter <elethomhunter@gmail.com>
2016-01-25 20:48:53 +0800

    Add ResetRituals
    Reset daily, weekly, monthly, annual rituals. Week starts from Monday by default.

---
+++ ResetRituals.js
@@ -0,0 +1,70 @@
+of = Library('OmniFocus')
+weekStartFromMonday = true
+
+dailyTasks = of.tasksWithContext('Daily')
+dailyTasks.forEach(function(task) {
+    dueDate = task.dueDate()
+    date = new Date
+    if (dueDate.getFullYear() < date.getFullYear() ||
+        (dueDate.getFullYear() === date.getFullYear() && dueDate.getMonth() < date.getMonth()) ||
+        (dueDate.getFullYear() === date.getFullYear() && dueDate.getMonth() === date.getMonth() && dueDate.getDate() < date.getDate())) {
+        date.setHours(task.dueDate().getHours())
+        date.setMinutes(task.dueDate().getMinutes())
+        task.dueDate = date
+        task.completed = false
+    }
+})
+
+function dayOfWeek(date) {
+    if (weekStartFromMonday) {
+        return (date.getDay() + 6) % 7
+    } else {
+        return date.getDay()
+    }
+}
+weeklyTasks = of.tasksWithContext('Weekly')
+weeklyTasks.forEach(function(task) {
+    dueDate = task.dueDate()
+    date = new Date
+    dueDay = dayOfWeek(dueDate)
+    console.log(dayOfWeek(date))
+    dueDate.setDate(dueDate.getDate() - dueDay)
+    date.setDate(date.getDate() - dayOfWeek(date))
+    if (dueDate.getFullYear() < date.getFullYear() ||
+        (dueDate.getFullYear() === date.getFullYear() && dueDate.getMonth() < date.getMonth()) ||
+        (dueDate.getFullYear() === date.getFullYear() && dueDate.getMonth() === date.getMonth() && dueDate.getDate() < date.getDate())) {
+        date.setDate(date.getDate() + dueDay)
+        date.setHours(dueDate.getHours())
+        date.setMinutes(dueDate.getMinutes())
+        task.dueDate = date
+        task.completed = false
+    }
+})
+
+monthlyTasks = of.tasksWithContext('Monthly')
+monthlyTasks.forEach(function(task) {
+    dueDate = task.dueDate()
+    date = new Date
+    if (dueDate.getFullYear() < date.getFullYear() ||
+        (dueDate.getFullYear() === date.getFullYear() && dueDate.getMonth() < date.getMonth())) {
+        date.setDate(dueDate.getDate())
+        date.setHours(dueDate.getHours())
+        date.setMinutes(dueDate.getMinutes())
+        task.dueDate = date
+        task.completed = false
+    }
+})
+
+annualTasks = of.tasksWithContext('Annual')
+annualTasks.forEach(function(task) {
+    dueDate = task.dueDate()
+    date = new Date
+    if (dueDate.getFullYear() < date.getFullYear()) {
+        date.setMonth(dueDate.getMonth())
+        date.setDate(dueDate.getDate())
+        date.setHours(dueDate.getHours())
+        date.setMinutes(dueDate.getMinutes())
+        task.dueDate = date
+        task.completed = false
+    }
+})
